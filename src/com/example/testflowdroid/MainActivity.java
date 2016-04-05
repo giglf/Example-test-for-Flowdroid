b7217d6f275e6119cb19a3959e2d92a662670cd9
giglf <lin.giglf@gmail.com>
2016-04-05 23:25:12 +0800

    a bit modify

--- src/com/example/testflowdroid/MainActivity.java
+++ src/com/example/testflowdroid/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends Activity {
                 null, message, null, null);
     }
 
-    @Override
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
