253db911228068531bcc8dd843ea32cc0653fe76
giglf <lin.giglf@gmail.com>
2016-04-06 10:40:41 +0800

    enhance 2enExample according to alias analyse from author website

--- src/com/example/testflowdroid/MainActivity.java
+++ src/com/example/testflowdroid/MainActivity.java
@@ -5,22 +5,41 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 
+class A{
+	public B atob;
+	A(){
+		atob = new B();
+	}
+}
+
+class B{
+	public int num;
+}
 
 public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		source();
-		int b = 2;
+
 		
-		sink(b);
+		A a = new A();
+		B b = a.atob;
+		foo(a);
+		sink(b.num);
+		
 	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu){
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void foo(A aa){
+		B tmp = aa.atob;
+		int w = source();
+		tmp.num = w;
 	}
 	
 	public int source(){
